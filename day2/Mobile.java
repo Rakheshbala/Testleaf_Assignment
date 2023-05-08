4c36f66accd708239ccffd28a50f370991def0b5
Rakheshbala <84392452+Rakheshbala@users.noreply.github.com>
2023-05-08 20:33:55 +0530

    Add files via upload
    Day2

---
+++ day2/Mobile.java
@@ -0,0 +1,30 @@
+package week1.day2;
+
+public class Mobile {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String mobileBrandName="Oneplus";
+		System.out.println(mobileBrandName);
+		
+		char mobileLogo='O';
+		System.out.println(mobileLogo);
+		
+		short noOfMobilePiece=1;
+		System.out.println(noOfMobilePiece);
+		
+		int  modelNumber=11;
+		System.out.println(modelNumber);
+		
+		long mobileImeiNumber=979894959;
+		System.out.println(mobileImeiNumber);
+		
+		float mobilePrice=49999.99f;
+		System.out.println(mobilePrice);
+		
+		boolean isDamaged=false;
+		System.out.println(isDamaged);
+
+	}
+
+}
