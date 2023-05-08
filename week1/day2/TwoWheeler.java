935f0d91859e0f3d60fa280815cfc4c1995d41bd
Rakheshbala <84392452+Rakheshbala@users.noreply.github.com>
2023-05-08 20:35:33 +0530

    Add files via upload
    Day2

---
+++ week1/day2/TwoWheeler.java
@@ -0,0 +1,27 @@
+package week1.day2;
+
+public class TwoWheeler {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int  noOfWheels=04;
+		System.out.println(noOfWheels);
+		
+		short noOfMirrors=2;
+		System.out.println(noOfMirrors);
+		
+		long chassisNumber=987654321L;
+		System.out.println(chassisNumber);
+		
+		boolean isPunctured=true;
+		System.out.println(isPunctured);
+		
+		String bikeName="Ninja";
+		System.out.println(bikeName);
+		
+		double runningKM=987.654d;
+		System.out.println(runningKM);
+
+	}
+
+}
