47feb2d91f58b18a17f6f8719a74512bfe6b895f
Rakheshbala <84392452+Rakheshbala@users.noreply.github.com>
2023-05-18 02:16:10 +0530

    Add files via upload
    week2

---
+++ week2/day1/LargestNumber.java
@@ -0,0 +1,15 @@
+package week2.day1;
+
+public class LargestNumber {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int[]num= {1,2,3,4,6,8,9};
+		int len= num.length;
+		System.out.println(len);
+		System.out.println(num[len-2]);
+		
+
+	}
+
+}
